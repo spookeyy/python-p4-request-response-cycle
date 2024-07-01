4cec8c587767725859f8f3a4820bdd466721aa4e
spookeyy <pangasmeshack@gmail.com>
2024-07-01 12:09:32 +0300

    done

--- server/app.py
+++ server/app.py
@@ -1,8 +1,34 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, request, current_app, g, make_response
 
 app = Flask(__name__)
+
+@app.before_request
+def app_path():
+    g.path = os.path.abspath(os.getcwd())
+
+@app.route('/')
+def index():
+    host = request.headers.get('Host')
+    appname = current_app.name
+    response_body = f'''
+    <html>
+        <head>
+            <title>App: {appname}</title>
+        </head>
+        <body>
+            <h1>The name of this application is {appname}</h1>
+            <p>The Host for this page: {host}</p>
+            <p>The path for this page: {g.path}</p>
+        </body>
+    </html>
+    '''
+
+    status_code = 200
+    headers = {}
+
+    return make_response(response_body, status_code, headers)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
